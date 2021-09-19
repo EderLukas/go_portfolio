9eb5f12080909f6ba4c5c46bc5fa7b2463292aae
Lukas Eder <eder.lukas@bluewin.ch>
2021-09-19 17:17:28 +0200

    Further implementation and first debugging of UpdatePlayingField.

--- gameOfLife/main/main.go
+++ gameOfLife/main/main.go
@@ -1,34 +1,37 @@
 package main
 
 import (
-	"fmt"
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 	"image/color"
-	"reflect"
+	"time"
 )
 // Game implements ebiten.Game interface
 type Game struct{}
 
 // PlayingField Store
-type PlayingField [100][100]struct{
+type PlayingField [10][10]struct{
 	xCoord float64
 	yCoord float64
 }
 
-var playingField = &PlayingField{
-0:{1:{4,0}},
-1:{2:{8,4}},
-3:{0:{0,8}, 1:{4,8}, 2:{8,8}},
+var playingField = PlayingField{
+0:{1:{40,0}},
+1:{2:{80,40}},
+2:{0:{0,80}, 1:{40,80}, 2:{80,80}},
 }
 
+var firstIteration bool = true
 
 // Update proceeds the game state.
 // Update is called every tick (1/60 [s] by default).
 func (g *Game) Update () error {
 	// Write your game's logical update.
-	UpdatePlayingField(playingField)
-
+	if !firstIteration {
+		UpdatePlayingField(&playingField)
+	}
+	firstIteration = false
+	time.Sleep(1 * time.Second)
 	return nil
 }
 
@@ -36,9 +39,9 @@ func (g *Game) Update () error {
 // Draw is called every frame (typically 1/60[s] for 60Hz display).
 func (g *Game) Draw(screen *ebiten.Image) {
 	screen.Fill(color.White)
-	for _, line := range playingField {
+	for _, line := range &playingField {
 		for _, cell := range line {
-			ebitenutil.DrawRect(screen, cell.xCoord, cell.yCoord, 4, 4,  color.Black)
+			ebitenutil.DrawRect(screen, cell.xCoord, cell.yCoord, 40, 40,  color.Black)
 		}
 	}
 }
@@ -62,25 +65,80 @@ func main() {
 
 // The game of life algorithm
 func UpdatePlayingField (pf *PlayingField) {
-	lineIndex := 0
-	columnIndex := 0
+	var yCoord float64 = 0
+	for i, line := range pf {
+		var xCoord float64 = 0
 
-	for _, line := range pf {
-		for _, column := range line{
-			fmt.Printf("%s\n", reflect.TypeOf(column))
-			isCellAlive := CheckIsCellAlive(column)
+		for j, column := range line{
+			isCellAlive := CheckIsCellAlive(column.xCoord, column.yCoord)
+			if !isCellAlive {
+				xCoord += 40
+				continue
+			} else if isCellAlive {
+				numOfNeighbors := CheckNumberOfNeighbors(i, j, pf)
 
-
-			columnIndex += 4
+				if numOfNeighbors < 2 || numOfNeighbors > 3 {
+					pf[i][j].xCoord = 0
+					pf[i][j].yCoord = 0
+				} else {
+					pf[i][j].xCoord = xCoord
+					pf[i][j].yCoord = yCoord
+				}
+			}
+			xCoord += 40
 		}
-		lineIndex += 4
+		yCoord += 40
 	}
 }
 
-// TODO find out what type cell is => column is of type struct { xCoord float64; yCoord float64 }
-func CheckIsCellAlive (cell) bool {
-	if (cell.xCoord > 0 && cell.yCoord > 0) {
+func CheckIsCellAlive (x float64, y float64 ) bool {
+	if x > 0 || y > 0 {
 		return true
 	}
 	return false
+}
+
+func CheckNumberOfNeighbors (lineIndex int, columnIndex int, field *PlayingField) int {
+	neighborsCount := 0
+	if lineIndex > 0 && columnIndex > 0 {
+		if field[lineIndex-1][columnIndex-1].xCoord > 0 &&
+			field[lineIndex-1][columnIndex-1].yCoord > 0 {
+			neighborsCount += 1
+		}
+	}
+	if lineIndex > 0 {
+		if field[lineIndex-1][columnIndex].xCoord > 0 &&
+			field[lineIndex-1][columnIndex].yCoord > 0 {
+			neighborsCount += 1
+		}
+		if field[lineIndex-1][columnIndex+1].xCoord > 0 &&
+			field[lineIndex-1][columnIndex+1].yCoord > 0 {
+			neighborsCount += 1
+		}
+	}
+	if columnIndex > 0 {
+		if field[lineIndex][columnIndex-1].xCoord > 0 &&
+			field[lineIndex][columnIndex-1].yCoord > 0 {
+			neighborsCount += 1
+		}
+	}
+	if field[lineIndex][columnIndex + 1].xCoord > 0 &&
+		field[lineIndex][columnIndex + 1].yCoord > 0 {
+		neighborsCount += 1
+	}
+	if columnIndex > 0 {
+		if field[lineIndex+1][columnIndex-1].xCoord > 0 &&
+			field[lineIndex+1][columnIndex-1].yCoord > 0 {
+			neighborsCount += 1
+		}
+	}
+	if field[lineIndex + 1][columnIndex].xCoord > 0 &&
+		field[lineIndex + 1][columnIndex].yCoord > 0 {
+		neighborsCount += 1
+	}
+	if field[lineIndex + 1][columnIndex + 1].xCoord > 0 &&
+		field[lineIndex + 1][columnIndex + 1].yCoord > 0 {
+		neighborsCount += 1
+	}
+	return neighborsCount
 }
