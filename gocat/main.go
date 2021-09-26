2ecca2c227575ab7c236eea157f2ccb047f80359
Lukas Eder <eder.lukas@bluewin.ch>
2021-09-26 12:01:32 +0200

    implemented gocat script

--- gocat/main.go
+++ gocat/main.go
@@ -1,1 +1,29 @@
-package gocat
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+func main() {
+	if len(os.Args) == 1 {
+		fmt.Println("There must be at least one file name given as a parameter!")
+		os.Exit(1)
+	}
+
+	for _, fname := range os.Args[1:] {
+		f, err := os.Open(fname)
+		if err != nil {
+			log.Printf("Error occured when opening file: %s\n%s", fname, err)
+			f.Close()
+			continue
+		}
+		_, err = io.Copy(os.Stdout, f)
+		if err != nil {
+			log.Printf("Error on output %s\n%s", fname, err)
+		}
+		f.Close()
+	}
+}
